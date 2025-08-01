ea089014b423132c6b5376f045e6f0f3878a9fc0
M_HUZAIFA ASIF <asifhuzaifa328@gmail.com>
2025-08-01 17:51:29 +0500

    generate HTML report

--- cypress.config.js
+++ cypress.config.js
@@ -2,5 +2,16 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+   "reporter":"mochawesome",
+    "reporterOptions":{
+      "charts":"true",
+      "overwrite":"false",
+      "html":"true",
+      "json":"true",
+      "repDir":"cypress/reports"
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   }
 });
